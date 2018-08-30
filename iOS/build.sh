4216faa8a5ba0947a7fbbefefb96b9da494cdd6a
root <425880365@qq.com>
2018-08-30 18:17:20 +0800

    disable bitcode

--- iOS/build.sh
+++ iOS/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PROJ_PATH=$1
 xcodebuild clean -project $PROJ_PATH
-xcodebuild -configuration Release -sdk iphoneos ARCHS="arm64" IPHONEOS_DEPLOYMENT_TARGET="10.3" -project $PROJ_PATH
+xcodebuild -configuration Release -sdk iphoneos ARCHS="arm64 armv7" IPHONEOS_DEPLOYMENT_TARGET="10.3" ENABLE_BITCODE="NO" -project $PROJ_PATH
